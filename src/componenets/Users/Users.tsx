57838a264423556c88d2044eeb81647a757ce08c
zanzipanziby <zanzipanziby@gmail.com>
2023-01-21 00:03:01 +0300

    lesson-52--side-effect

--- src/componenets/Users/Users.tsx
+++ src/componenets/Users/Users.tsx
@@ -17,121 +17,124 @@ type UsersPropsType = {
     changeFollowStatus: (id: number, followStatus: boolean) => void
 }
 export const Users = (props: UsersPropsType) => {
-    // useEffect(()=> {
-    //     props.setUsers([
-    //         {
-    //             id: new Date().getTime().toString(),
-    //             avatar: avatarMisha,
-    //             fullName: "Misha",
-    //             status: "I'm the son of a boss",
-    //             followed: true,
-    //             location: {
-    //                 city: "Vileyka",
-    //                 country: "Belarus"
-    //             }
-    //         },
-    //         {
-    //             id: new Date().getTime().toString(),
-    //             avatar: avatarVeronika,
-    //             fullName: "Veronika", status: "I'm the wife of a boss",
-    //             followed: true,
-    //             location: {
-    //                 city: "Vileyka",
-    //                 country: "Belarus"
-    //             }
-    //         },
-    //         {
-    //             id: new Date().getTime().toString(),
-    //             avatar: avatarJura,
-    //             fullName: "Jura", status: "I'm the son of a boss",
-    //             followed: true,
-    //             location: {
-    //                 city: "Vileyka",
-    //                 country: "Belarus"
-    //             }
-    //         },
-    //         {
-    //             id: new Date().getTime().toString(),
-    //             avatar: avatarSvetlana,
-    //             fullName: "Svetlana", status: "I'm the mom of a boss",
-    //             followed: true,
-    //             location: {
-    //                 city: "Smilovichi",
-    //                 country: "Belarus"
-    //             }
-    //         },
-    //         {
-    //             id: new Date().getTime().toString(),
-    //             avatar: avatarOzzy,
-    //             fullName: "Ozzy", status: "I'm rock-man",
-    //             followed: false,
-    //             location: {
-    //                 city: "Miami",
-    //                 country: "USA"
-    //             }
-    //         },
-    //     ])
-    // },[])
-    if (props.users.length === 0) {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users')
-            .then(res=>{
-                props.setUsers(res.data.items)
-            })
+    let getUsers = () => {
+        // useEffect(()=> {
+        //     props.setUsers([
+        //         {
+        //             id: new Date().getTime().toString(),
+        //             avatar: avatarMisha,
+        //             fullName: "Misha",
+        //             status: "I'm the son of a boss",
+        //             followed: true,
+        //             location: {
+        //                 city: "Vileyka",
+        //                 country: "Belarus"
+        //             }
+        //         },
+        //         {
+        //             id: new Date().getTime().toString(),
+        //             avatar: avatarVeronika,
+        //             fullName: "Veronika", status: "I'm the wife of a boss",
+        //             followed: true,
+        //             location: {
+        //                 city: "Vileyka",
+        //                 country: "Belarus"
+        //             }
+        //         },
+        //         {
+        //             id: new Date().getTime().toString(),
+        //             avatar: avatarJura,
+        //             fullName: "Jura", status: "I'm the son of a boss",
+        //             followed: true,
+        //             location: {
+        //                 city: "Vileyka",
+        //                 country: "Belarus"
+        //             }
+        //         },
+        //         {
+        //             id: new Date().getTime().toString(),
+        //             avatar: avatarSvetlana,
+        //             fullName: "Svetlana", status: "I'm the mom of a boss",
+        //             followed: true,
+        //             location: {
+        //                 city: "Smilovichi",
+        //                 country: "Belarus"
+        //             }
+        //         },
+        //         {
+        //             id: new Date().getTime().toString(),
+        //             avatar: avatarOzzy,
+        //             fullName: "Ozzy", status: "I'm rock-man",
+        //             followed: false,
+        //             location: {
+        //                 city: "Miami",
+        //                 country: "USA"
+        //             }
+        //         },
+        //     ])
+        // },[])
+        if (props.users.length === 0) {
+            axios.get('https://social-network.samuraijs.com/api/1.0/users')
+                .then(res => {
+                    props.setUsers(res.data.items)
+                })
 
-        // props.setUsers([
-        //     {
-        //         id: v1(),
-        //         avatar: avatarMisha,
-        //         fullName: "Misha",
-        //         status: "I'm the son of a boss",
-        //         followed: true,
-        //         location: {
-        //             city: "Vileyka",
-        //             country: "Belarus"
-        //         }
-        //     },
-        //     {
-        //         id: v1(),
-        //         avatar: avatarVeronika,
-        //         fullName: "Veronika", status: "I'm the wife of a boss",
-        //         followed: true,
-        //         location: {
-        //             city: "Vileyka",
-        //             country: "Belarus"
-        //         }
-        //     },
-        //     {
-        //         id: v1(),
-        //         avatar: avatarJura,
-        //         fullName: "Jura", status: "I'm the son of a boss",
-        //         followed: true,
-        //         location: {
-        //             city: "Vileyka",
-        //             country: "Belarus"
-        //         }
-        //     },
-        //     {
-        //         id: v1(),
-        //         avatar: avatarSvetlana,
-        //         fullName: "Svetlana", status: "I'm the mom of a boss",
-        //         followed: true,
-        //         location: {
-        //             city: "Smilovichi",
-        //             country: "Belarus"
-        //         }
-        //     },
-        //     {
-        //         id: v1(),
-        //         avatar: avatarOzzy,
-        //         fullName: "Ozzy", status: "I'm rock-man",
-        //         followed: false,
-        //         location: {
-        //             city: "Miami",
-        //             country: "USA"
-        //         }
-        //     },
-        // ])
+            // props.setUsers([
+            //     {
+            //         id: v1(),
+            //         avatar: avatarMisha,
+            //         fullName: "Misha",
+            //         status: "I'm the son of a boss",
+            //         followed: true,
+            //         location: {
+            //             city: "Vileyka",
+            //             country: "Belarus"
+            //         }
+            //     },
+            //     {
+            //         id: v1(),
+            //         avatar: avatarVeronika,
+            //         fullName: "Veronika", status: "I'm the wife of a boss",
+            //         followed: true,
+            //         location: {
+            //             city: "Vileyka",
+            //             country: "Belarus"
+            //         }
+            //     },
+            //     {
+            //         id: v1(),
+            //         avatar: avatarJura,
+            //         fullName: "Jura", status: "I'm the son of a boss",
+            //         followed: true,
+            //         location: {
+            //             city: "Vileyka",
+            //             country: "Belarus"
+            //         }
+            //     },
+            //     {
+            //         id: v1(),
+            //         avatar: avatarSvetlana,
+            //         fullName: "Svetlana", status: "I'm the mom of a boss",
+            //         followed: true,
+            //         location: {
+            //             city: "Smilovichi",
+            //             country: "Belarus"
+            //         }
+            //     },
+            //     {
+            //         id: v1(),
+            //         avatar: avatarOzzy,
+            //         fullName: "Ozzy", status: "I'm rock-man",
+            //         followed: false,
+            //         location: {
+            //             city: "Miami",
+            //             country: "USA"
+            //         }
+            //     },
+            // ])
+        }
     }
+
 
 
     let usersRender = props.users
@@ -165,12 +168,12 @@ export const Users = (props: UsersPropsType) => {
                 <li key={u.id} className={s.user}>
                     <div className={s.avatarAndButtonContainer}>
                         <div className={s.avatarContainer}>
-                            <img src= {u.photos.small ? u.photos.small : avatarJura} alt={u.name}/>
+                            <img src={u.photos.small ? u.photos.small : avatarJura} alt={u.name}/>
                         </div>
                         <div>
                             <button
                                 className={s.followButton}
-                                onClick={()=>props.changeFollowStatus(u.id, !u.followed)}
+                                onClick={() => props.changeFollowStatus(u.id, !u.followed)}
                             >
                                 {u.followed ? 'Unfollow' : 'Follow'}
                             </button>
@@ -193,6 +196,7 @@ export const Users = (props: UsersPropsType) => {
     return (
         <div>
             <h3 className={s.title}>Users</h3>
+            <button onClick={getUsers}>Get Users</button>
             <ul>
                 {usersRender}
             </ul>
