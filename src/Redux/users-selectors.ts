4ea8447de9d1697c67de95295d0548174221b714
zanzipanziby <zanzipanziby@gmail.com>
2023-04-10 19:57:15 +0300

    lesson-82-83---reselect(createSelector)-part-2&part-3 file user-selector line 8

--- src/Redux/users-selectors.ts
+++ src/Redux/users-selectors.ts
@@ -1,21 +1,24 @@
-import {RootStateType} from "./StateAndActionTypes";
+import {RootStateType, UserTypeWithoutServer} from "./StateAndActionTypes";
+import {createSelector} from "reselect";
 
-export const getUsers = (state:RootStateType) => {
+export const getUsers = (state: RootStateType) => {
     return state.usersPage.users
 }
+//   -------------------   Use Library reselect  ------------------------\\
+export const getUsersSuper = createSelector(getUsers, (users:  UserTypeWithoutServer[]) => users.map(u => u))
 
-export const getPageSize = (state:RootStateType) => {
+export const getPageSize = (state: RootStateType) => {
     return state.usersPage.pageSize
 }
-export const getTotalUsersCount = (state:RootStateType) => {
+export const getTotalUsersCount = (state: RootStateType) => {
     return state.usersPage.totalUsersCount
 }
-export const getCurrentPage = (state:RootStateType) => {
+export const getCurrentPage = (state: RootStateType) => {
     return state.usersPage.currentPage
 }
-export const getIsFetching = (state:RootStateType) => {
+export const getIsFetching = (state: RootStateType) => {
     return state.usersPage.isFetching
 }
-export const getFollowingInProgress = (state:RootStateType) => {
+export const getFollowingInProgress = (state: RootStateType) => {
     return state.usersPage.followingInProgress
 }
